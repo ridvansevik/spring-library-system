392fcea84726a1f1e40219d4839d2b2b16ab91e9
Ridvan Sevik <ridvan.sevik.tr@gmail.com>
2025-06-27 12:08:25 +0300

    feat : add loan class for return and borrow logic.

---
+++ src/main/java/com/ridvansevik/library_app/model/Loan.java
@@ -0,0 +1,39 @@
+package com.ridvansevik.library_app.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id",nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    private LocalDate loanDate;
+
+    private LocalDate dueDate;
+
+    private LocalDate returnDate;
+
+    public Loan(Book book,User user){
+        this.book=book;
+        this.user=user;
+        this.loanDate=LocalDate.now();
+        this.dueDate=LocalDate.now().plusWeeks(2);
+    }
+}
