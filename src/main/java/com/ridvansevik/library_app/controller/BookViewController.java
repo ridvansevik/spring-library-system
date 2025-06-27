bf2f557a74370868b93f5b3a52c04b5456ef6535
Ridvan Sevik <ridvan.sevik.tr@gmail.com>
2025-06-27 12:11:14 +0300

    feat: add borrow and return endpoints to BookViewController

--- src/main/java/com/ridvansevik/library_app/controller/BookViewController.java
+++ src/main/java/com/ridvansevik/library_app/controller/BookViewController.java
@@ -3,11 +3,14 @@ package com.ridvansevik.library_app.controller;
 
 import com.ridvansevik.library_app.model.Book;
 import com.ridvansevik.library_app.service.BookService;
+import com.ridvansevik.library_app.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/web")
@@ -15,6 +18,7 @@ import java.security.Principal;
 public class BookViewController {
 
     private final BookService bookService;
+    private final LoanService loanService;
 
     @GetMapping("/books")
     public String getAllBooks(Model model , Principal principal){
@@ -22,7 +26,12 @@ public class BookViewController {
         if(principal != null){
             model.addAttribute("username", principal.getName());
         }
+
+        Map<Long,String> borrowersMap = loanService.findActiveLoans().stream()
+                        .collect(Collectors.toMap(loan -> loan.getBook().getId(),loan -> loan.getUser().getUsername()));
+
         model.addAttribute("books",bookService.getAllBooks());
+        model.addAttribute("borrowersMap", borrowersMap);
         return "books";
     }
 
@@ -60,5 +69,23 @@ public class BookViewController {
     }
 
 
+    @PostMapping("books/borrow/{id}")
+    public String borrowBook(@PathVariable Long id,Principal principal){
+
+        if(principal != null){
+            loanService.borrowBook(id,principal.getName());
+        }
+
+        return "redirect:/web/books";
+
+    }
+
+    @PostMapping("books/return/{id}")
+    public String returnBook(@PathVariable Long id, Principal principal){
+        if (principal != null) {
+            loanService.returnBook(id, principal.getName());
+        }
+        return "redirect:/web/books";
+    }
 
 }
