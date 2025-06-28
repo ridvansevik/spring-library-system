a65a0548ade2e4e38f08dfe40e2cd0028dc70c64
Ridvan Sevik <ridvan.sevik.tr@gmail.com>
2025-06-28 08:27:01 +0300

    perf: add @EntityGraph to LoanRepository queries for optimized fetching

--- src/main/java/com/ridvansevik/library_app/repository/LoanRepository.java
+++ src/main/java/com/ridvansevik/library_app/repository/LoanRepository.java
@@ -1,16 +1,23 @@
 package com.ridvansevik.library_app.repository;
 
 import com.ridvansevik.library_app.model.Loan;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface LoanRepository extends JpaRepository<Loan,Long> {
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 
-Optional<Loan> findByBookIdAndReturnDateIsNull(Long bookId);
+
+    @EntityGraph(attributePaths = {"book", "user"})
+    Optional<Loan> findByBookIdAndReturnDateIsNull(Long bookId);
+
+    @EntityGraph(attributePaths = {"book", "user"})
     List<Loan> findByReturnDateIsNull();
+
     boolean existsByBookId(Long bookId);
 
+    @EntityGraph(attributePaths = {"book", "user"})
     List<Loan> findByUser_Username(String username);
 }
