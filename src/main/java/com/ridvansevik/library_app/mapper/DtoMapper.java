fd68965b5d64d94bc76271a5bcc5c1527794079d
Ridvan Sevik <ridvan.sevik.tr@gmail.com>
2025-06-28 08:25:12 +0300

    feat: implement DtoMapper to convert entity classes to DTOs

---
+++ src/main/java/com/ridvansevik/library_app/mapper/DtoMapper.java
@@ -0,0 +1,48 @@
+package com.ridvansevik.library_app.mapper;
+
+import com.ridvansevik.library_app.dto.BookDto;
+import com.ridvansevik.library_app.dto.LoanDto;
+import com.ridvansevik.library_app.dto.UserDto;
+import com.ridvansevik.library_app.model.Book;
+import com.ridvansevik.library_app.model.Loan;
+import com.ridvansevik.library_app.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DtoMapper {
+
+public UserDto toUserDto(User user){
+    if(user==null){
+        return null;
+    }
+    UserDto userDto = new UserDto();
+    userDto.setId(user.getId());
+    userDto.setRole(user.getRole());
+    userDto.setUsername(user.getUsername());
+    return userDto;
+}
+
+
+public BookDto toBookDto(Book book){
+    if(book==null) return null;
+    BookDto bookDto = new BookDto();
+    bookDto.setId(book.getId());
+    bookDto.setBookStatus(book.getBookStatus());
+    bookDto.setIsbn(book.getIsbn());
+    bookDto.setAuthor(book.getAuthor());
+    bookDto.setTitle(book.getAuthor());
+    return bookDto;
+}
+
+public LoanDto toLoanDto(Loan loan){
+    if(loan==null) return null;
+    LoanDto loanDto = new LoanDto();
+    loanDto.setBook(toBookDto(loan.getBook()));
+    loanDto.setId(loan.getId());
+    loanDto.setUser(toUserDto(loan.getUser()));
+    loanDto.setLoanDate(loan.getLoanDate());
+    loanDto.setDueDate(loan.getDueDate());
+    loanDto.setReturnDate(loan.getReturnDate());
+    return loanDto;
+}
+}
