841db8faa65b893c3a2951ca9df47dd416d7354e
Ridvan Sevik <ridvan.sevik.tr@gmail.com>
2025-07-06 10:32:51 +0300

    feat: add soft delete functionality to Book entity using @SQLDelete and @Where

--- src/main/java/com/ridvansevik/library_app/model/Book.java
+++ src/main/java/com/ridvansevik/library_app/model/Book.java
@@ -5,25 +5,33 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE book SET deleted = true WHERE id=?") // Intercepts delete commands
+@Where(clause = "deleted=false") // Ensures all queries only find non-deleted books
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotBlank(message = "Başlık alanı boş olamaz")
     private String title;
+
     @NotBlank(message = "Yazar alanı boş olamaz")
     private String author;
+
     private String isbn;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private BookStatus bookStatus=BookStatus.AVAILABLE;
+    private BookStatus bookStatus = BookStatus.AVAILABLE;
 
-
+    @Column(columnDefinition = "boolean default false")
+    private boolean deleted = Boolean.FALSE;
 }
